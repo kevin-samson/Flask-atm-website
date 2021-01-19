ae11197ff7fd022673e1fcc03822945ffb08cf5a
Kevin Samson <kevinsam2003@gmail.com>
2021-01-19 17:24:21 +0400

    Removed Stuff

--- atm/bank/forms.py
+++ atm/bank/forms.py
@@ -3,14 +3,6 @@ from wtforms import SubmitField, IntegerField
 from wtforms.validators import NumberRange, DataRequired
 
 
-class CusForm(FlaskForm):
-    b50 = SubmitField('50')
-    b100 = SubmitField('100')
-    b250 = SubmitField('250')
-    b500 = SubmitField('500')
-    b1000 = SubmitField('1000')
-
-
 class Money(FlaskForm):
     num = IntegerField('', validators=[NumberRange(1, 100000)])
     submit = SubmitField('Submit')
--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-import mysql.connector
-
-from atm.main.other import acc_no
-
-mydb = mysql.connector.connect(host="localhost", user="root", passwd="123456", database="atm1",
-                               auth_plugin='mysql_native_password')
-cur = mydb.cursor(buffered=True)
-
-def view_logs():
-
-    cur.execute(f"select * from transactions where id=35 order by date desc;select count(*) from logs")
-    mydb.commit()
-    return cur.fetchall()
-
-print(view_logs())
+STRING = "WELCOME"
+NOTE = ""
+for S in range(0,4):
+    print(STRING[S])
+    print(S,STRING)
